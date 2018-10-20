c2a4f29b461c0ee4e68a6dd36e2d06517908230d
Umang Dhandhania <umangd98@gmail.com>
2018-10-20 19:23:12 +0530

    BLOG FUNCTIONALITY AND POLISH

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,6 +125,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static/')
+]
+
+STATIC_ROOT =os.path.join(BASE_DIR, 'static')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
